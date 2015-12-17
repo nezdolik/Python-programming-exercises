b76a71112df7e72f6a858d048464bb66efa3dd21
Kateryna Nezdolii <kateryna.nezdolii@gmail.com>
2015-12-17 16:47:48 +0100

    Update pe.py

--- pe.py
+++ pe.py
@@ -517,7 +517,35 @@ def question_23():
     print squareStr%(2,square(2))
     print squareStr%(3,square(3))
 
+'''
+#----------------------------------------#
+Question 24
+Level 1
 
+Question:
+    Python has many built-in functions, and if you do not know how to use it, you can read document online or find some books. But Python has a built-in document function for every built-in functions.
+    Please write a program to print some Python built-in functions documents, such as abs(), int(), raw_input()
+    And add document for your own function
+'''
+def custom_func(word):
+    """ custom_func(word) -> string.
+
+    Function prints passed word parameter to console and returns word in upper case.
+
+    Args:
+         word (string): string to be printed to console
+
+    Returns:
+        string: word in upper case
+    """
+    print word
+    return word.upper()
+
+def question_24():
+    print abs.__doc__
+    print int.__doc__
+    print raw_input.__doc__
+    print custom_func.__doc__
 
 
 
